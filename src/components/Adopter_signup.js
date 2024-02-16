4b7965ab74fd11fa23dae6be6f101a6057fcc17a
Priya <priyajoshi0206@gmail.com>
2024-02-16 22:07:42 +0530

    local update

--- src/components/Adopter_signup.js
+++ src/components/Adopter_signup.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Adopter_signup() {
   return (
    <>
-<div class="bg-grey-lighter min-h-screen flex flex-col">
+<div class="bg-orange-300 min-h-screen flex flex-col ">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                     <h1 className="mb-8 text-3xl text-center">Sign up</h1>
@@ -29,6 +29,12 @@ export default function Adopter_signup() {
                         className="block border border-grey-light w-full p-3 rounded mb-4"
                         name="confirm_password"
                         placeholder="Confirm Password" />
+
+                    <input 
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="Address"
+                        placeholder="Address" />
 
                     <button
                         type="submit"
