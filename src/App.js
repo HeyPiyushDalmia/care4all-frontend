2226fd04fc11510b75a0dafc147764297b9b5182
Piyush Dalmia <invictusgamer9900@gmail.com>
2024-02-17 00:57:57 +0530

    Error fixing

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import Header from './components/Header'
 // import Login from './components/Adopter_login'
 import About from './components/About'
 import Adopter_signup from './components/Adopter_signup'
-<<<<<<< HEAD
 import Adopt_signup from './components/Adopt_signup'
-
-=======
 import Adopter_login from './components/Adopter_login'
 import Ngo_login from './components/Ngo_login'
->>>>>>> 59aae32fe03a07c2f7a7d2e45b1c0c1dc0a4a4aa
+
 import './../src/assets/css/style.css'
 
 import { Routes as Switch, Route, BrowserRouter } from 'react-router-dom';
@@ -26,15 +23,12 @@ function App() {
     <Switch>
     <Route path='/' element={<Index/>}/>
     <Route path='/about' element={<About/>}/>
-<<<<<<< HEAD
     <Route path ='/adopter_signup' element={<Adopter_signup/>}/>
     <Route path ='/adopt_signup' element={<Adopt_signup/>}/>
-=======
     <Route path ='/Adopter_signup' element={<Adopter_signup/>}/>
     <Route path ='/Adopter_login' element={<Adopter_login/>}/>
     <Route path ='/Ngo_login' element={<Ngo_login/>}/>
     
->>>>>>> 59aae32fe03a07c2f7a7d2e45b1c0c1dc0a4a4aa
 
     </Switch>
     </BrowserRouter>
