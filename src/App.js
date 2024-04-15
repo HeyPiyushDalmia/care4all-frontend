c08ae730b92fd8ec4c71dddd44101405f6f1e458
Priya <priyajoshi0206@gmail.com>
2024-04-15 13:40:13 +0530

    Ngo profile creatiion

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Ngo_login from './components/Ngo_login'
 import Dashboard from './components/dashboard'
 import Adopt from './components/adopt'
 import Elearning from './components/elearning'
+import Ngo_profile from './components/Ngo_profile'
 import './../src/assets/css/style.css'
 
 import { Routes as Switch, Route, BrowserRouter } from 'react-router-dom';
@@ -32,6 +33,7 @@ function App() {
     <Route path = '/dashboard' element={<Dashboard/>}/>
     <Route path = "/adopt" element={<Adopt/>}/>
     <Route path = "/elearning" element={<Elearning/>}/>
+    <Route path="/Ngo_profile" element={<Ngo_profile/>}/>
     </Switch>
     </BrowserRouter>
      <Footer/>
---
+++ src/components/Ngo_profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Ngo_profile() {
+  return (
+    <>
+    <div className='ngo_profile_navbar'>
+       <frameset>
+        
+       </frameset>
+    </div>
+    </>
+  )
+}
+
